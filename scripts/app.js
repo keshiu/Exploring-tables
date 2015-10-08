465a927320c306f987329f1602f4a9ec10b4f0f9
Alena Arsenyeva <keshiu@list.ru>
2015-10-09 02:03:02 +0300

    Add data popup when click on row

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,27 @@
 "use strict";
-$(document).ready(function(){
-	$(".lighting td").on("mouseenter mouseleave", function(){
-        var td_index=$(this).index();
-        $(this).parents(".lighting").find("tr").each(function(){
-            $("td:eq("+td_index + ")",this).toggleClass("lighting_col");
-        });
-        $(this).toggleClass("lighting_cell");
+var PopUpShow = function () {
+	$("#table_popup").show();
+},
+PopUpHide = function () {
+	$("#table_popup").hide();
+}
+$( document ).ready( function() {
+	PopUpHide ();
+	$( ".lighting td" ).on( "mouseenter mouseleave", function() {
+        var td_index = $( this ).index();
+        $( this ).parents( ".lighting" ).find( "tr" ).each( function() {
+            $( "td:eq(" + td_index + ")", this ).toggleClass( "lighting_col" );
+        } );
+        $( this ).toggleClass( "lighting_cell" );
+	} );
+	$(".lighting tr").on("click", function() {
+		var tr = this,
+		cell = [];
+		for (var currentCol = 0; currentCol < 4; currentCol++) {
+			cell[currentCol] = tr.getElementsByTagName("td")[currentCol].innerHTML;
+		};
+		$("#content").empty();
+		$("#content").append(cell.join(" ; "));
+		PopUpShow();
 	});
-});
+} );
